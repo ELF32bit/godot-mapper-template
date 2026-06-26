4e0923e8a0dac32ad3b38ccbc18db01ac345cbda
ELF32bit <sergeybudyashov@gmail.com>
2026-06-26 17:25:04 +0300

    Enabled barycentric wireframes painting

--- mapping/builders/__post.gd
+++ mapping/builders/__post.gd
@@ -5,3 +5,31 @@ static func build(map: MapperMap) -> void:
 	var lightmap_gi := create_lightmap_gi(map, map.node)
 	var paths := preload("path_corner.gd").post_build(map)
 	preload("test_origami.gd").post_build_node_paths(map, paths)
+
+@warning_ignore("unused_parameter")
+static func build_faces_colors(face: MapperFace, colors: PackedColorArray) -> void:
+	if not face.parameters.size() >= 3: return
+	var source_colors := colors.duplicate()
+	var face_value := face.parameters[2]
+
+	# barycentric wireframes mode can be adjusted via special face flags
+	# colors.a = (16.0 - flags) / 16.0, where flags are [1], [2], [4], [8]
+	# [8] is the sign of face value distinguishing triangles from n-gons
+	# [1] will disable red vertex, [2] - green, [4] - blue
+	var ngon_flag: int = (8 if int(face_value) < 0 else 0)
+	face_value = str(absi(int(face_value)))
+
+	# face colors array can be resized to triangulate the face
+	colors.clear()
+	for index in range(1, source_colors.size() - 1):
+		colors.append(source_colors[0])
+		colors.append(source_colors[index])
+		colors.append(source_colors[index + 1])
+
+	# barycentric wireframes mode can be applied per triangle in the face
+	for index in range(mini(face_value.length(), source_colors.size() - 2)):
+		var vertex_flags := int(face_value[index]) % 8
+		var flags := (16.0 - float(vertex_flags | ngon_flag)) / 16.0
+		colors[index * 3 + 0].a = flags
+		colors[index * 3 + 1].a = flags
+		colors[index * 3 + 2].a = flags
